caae3ffaee3208bf01910286083dc98f57697d4e
Kayla Fang <kfang00@stuy.edu>
2018-11-21 14:49:17 -0500

    added format for methods

--- MyString.java
+++ MyString.java
@@ -1,6 +1,26 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
   public MyString(CharSequence s){
+    for (int a = 0; a < s.length(); a++) {
+      data[a] = s.charAt(a);
+    }
+  }
+
+  public char charAt(int index) {
 
   }
+
+  public int length() {
+
+  }
+
+  public CharSequence subSequence(int start, int end) {
+
+  }
+
+  public String toString() {
+
+  }
+
+
 }
