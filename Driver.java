2463ffc82fcc028a875752072f302fe098d428fb
Kayla Fang <kfang00@stuy.edu>
2018-11-25 15:18:10 -0500

    Added code for testing constructor

---
+++ Driver.java
@@ -0,0 +1,37 @@
+public class Driver {
+	public static void main(String[] args) {
+		MyString b,d;
+		String a,c;
+
+		// A list of test cases. Add anything to this list, and it will run through a complete test case on that MyString.
+		String[] cases = {
+			"",
+			"mario",
+			"luigi",
+			"qwertyuiopasdfghjklzxcvbnm",
+			"helpimtrappedinastringfactory"
+		};
+		// For compareTo(), every string will be compared to every other String (in both directions).
+
+		for (int i = 0; i < cases.length; i++) {
+			a = cases[i];
+		// Testing Constructor / MyString.toString()
+			try {
+				b = new MyString(a);
+				if (!a.equals(b.toString())) {
+						System.out.println("Error in Constructor / toString():");
+						System.out.println("\tInput:       \""+a+"\"");
+						System.out.println("\tYour result: \""+b+"\"");
+						return;
+				}
+			} catch (Exception e) {
+				System.out.println("Unwanted Exception in Constructor:");
+				System.out.println("\tInput: \""+a+"\"");
+				System.out.println("\nStack Trace:\n");
+				e.printStackTrace();
+				return;
+			}
+		}
+		System.out.println("Your code passed every test. Well done!");
+	}
+}
