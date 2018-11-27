4860a88a9feed932d9691ece034007233da7a34a
Kayla Fang <kfang00@stuy.edu>
2018-11-27 05:09:55 -0500

    Added more test cases

--- Driver.java
+++ Driver.java
@@ -102,6 +102,57 @@ public class Driver {
 			}
 		}
 		System.out.println("Your code passed every test. Well done!");
+    String refStr = "antidisestablishmentarianism";
+    MyString testStr = new MyString(refStr);
+    int c1 = 0;
+    if (testStr.length() != refStr.length()) {
+      System.out.println(++c1 + ". length() is " + testStr.length() + " instead of " + refStr.length());
+    }
+
+    for (int i = -1; i <= testStr.length(); i++) {
+      try {
+        if (testStr.charAt(i) != refStr.charAt(i)) {
+          System.out.println(++c1 + ". charAt() is returning the wrong char");
+          break;
+        }
+        if (i < 0 || i >= testStr.length()) {
+          System.out.println(++c1 + ". charAt(" + i + ") should throw an IndexOutOfBoundsException");
+        }
+      } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println(++c1 + ". charAt() should not throw an ArrayIndexOutOfBoundsException");
+      } catch (IndexOutOfBoundsException e) {
+      }
+    }
+
+    if (!testStr.toString().equals(refStr)) {
+      System.out.println(++c1 + ". testStr converts to " + testStr + " instead of " + refStr);
+    }
+
+    if (!testStr.subSequence(1,7).equals(refStr.subSequence(1,7))) {
+      System.out.println(++c1 + ". subSequence(1,7) is not " + refStr.subSequence(1,7));
+    }
+
+    if (testStr.compareTo(refStr) != 0) {
+      System.out.println(++c1 + ". compareTo() should return 0 in this case");
+    }
+
+    refStr += "abcd";
+
+    if (testStr.compareTo(refStr) >= 0) {
+      System.out.println(++c1 + ". compareTo() should return a negative in this case");
+    }
+
+    refStr = refStr.substring(0,20);
+
+    if (testStr.compareTo(refStr) <= 0) {
+      System.out.println(++c1 + ". compareTo() should return a positive in this case");
+    }
+
+    if (c1 == 0) {
+      System.out.println("\nYour MyString passed all the tests. Good work!");
+    } else {
+      System.out.println("\nYour MyString failed " + c1 + " tests. Keep trying!");
+    }
 	}
 
 	private static String sgn(int i) {
